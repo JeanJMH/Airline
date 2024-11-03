154bf9cf37d02e1f469f7c8c113b39a6ee9cc0c5
Jean Miranda <156374467+JeanJMH@users.noreply.github.com>
2024-11-03 09:36:02 -0500

    Feedbacktemplate.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,3 +11,28 @@ st.write(
 )
 
 os.environ["OPENAI_API_KEY"] = st.secrets["OpenAIkey"]
+
+feedback_type_template = """You are team support analyst. Analyze the following feedback text to determine if it inside the following clasification:
+1. Negative experience: for negative experiences caused by the airline's fault (for example lost luggage).
+2. Negative beyond control: for negative experiences beyond the airline's control (for example weather-related delays).
+3. Positive experience: for positive experiences.
+
+Respond only the clasification
+
+Text:
+{feedback}
+"""
+### Create the decision-making chain
+
+feedback_type_chain = (
+    PromptTemplate.from_template(feedback_type_template)
+    | llm
+    | StrOutputParser()
+)
+
+negative_airline_chain = PromptTemplate.from_template("""Respond with sympathies for the inconvenience caused by the airline. Inform the customer that customer service will contact them soon for resolution.
+
+Text:
+{text}
+""")| llm
+
