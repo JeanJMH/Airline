f25a23d5e3626ab822b3e8f0df7f78d3c0300ec5
Jean Miranda <156374467+JeanJMH@users.noreply.github.com>
2024-11-02 23:52:48 -0400

    Importlibraries.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,13 @@
 import streamlit as st
+from langchain.llms import OpenAI
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnableBranch
+import langchain
+import os
 
-st.title("🎈 My new app")
+st.title("Share with us your experience of the latest trip")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+os.environ["OPENAI_API_KEY"] = st.secrets["OpenAIkey"]
